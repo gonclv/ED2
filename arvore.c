a1ea6fcf9b3f6e9b1443b298e79a0d3b0300304e
Danilo Gonçalves <dangonc@proton.me>
2023-03-27 22:55:48 -0300

    fixed duplicates

--- arvore.c
+++ arvore.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 
 typedef struct no {
-<<<<<<< HEAD
     int chave;
     struct no *esq;
     struct no *dir;
@@ -126,28 +125,3 @@ void main() {
 
     emOrdem(raiz);
 }
-=======
-    int num;
-    struct no *esq;
-    struct no *dir;
-} TNo;
-
-int identificaFolha(TNo *no) {
-    if(!no) return 0;
-    if(!(no->esq) && !(no->dir)) return 1;
-    return 0;
-}
-
-void main() {
-    TNo *no = (TNo *) malloc(sizeof(TNo));
-    TNo *no2 = (TNo *) malloc(sizeof(TNo));
-    TNo *no3 = (TNo *) malloc(sizeof(TNo));
-    no->num = 1;
-    no->esq = NULL;
-    no->dir = no3;
-    if(identificaFolha(no) == 1)
-        printf("Nó é folha\n");
-    else
-        printf("Nó não é folha\n");
-}
->>>>>>> 479060d9b30d3119f13420306a68fc5ac83b280d
